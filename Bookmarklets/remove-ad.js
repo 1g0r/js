f12d181937543915644ffd01dd38e171d150beaa
1g0r <logovskoy@gmail.com>
2020-05-01 15:13:06 +0300

    Update and rename Bookmarclets/remove-ad.js to Bookmarklets/remove-ad.js

---
+++ Bookmarklets/remove-ad.js
@@ -0,0 +1,11 @@
+(function(){
+    var selector = '.__brand_top_2889, \
+    #mlph2889, \
+[class^="sidebar"]:not(html), \
+#fishki-video-frame-wrapper, \
+[class*="banner"], \
+iframe:not([allowfullscreen]), \
+.pagelet, \
+.sideColumn';
+    document.querySelectorAll(selector).forEach(e => e.remove());
+})()
