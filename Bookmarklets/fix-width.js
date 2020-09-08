be1a28d6ab76f2cc740907c730b4ab268229fc39
Игорь Логовской <igor@iMac-Igor.local>
2020-09-08 14:41:54 +0300

    More

--- Bookmarklets/fix-width.js
+++ Bookmarklets/fix-width.js
@@ -1,5 +1,9 @@
 (function(d){
-    var selector = '[class*=application],.l-table';
-    d.querySelectorAll(selector).forEach(e => e.style.width = '100%');
+    var selector = '[class*=application],.l-table,l-window';
+    d.querySelectorAll(selector).forEach(e => {
+        e.style.width = '100%';
+        e.style.position = 'absolute';
+        e.style.maxWidth = '100%';
+    });
     alert('done');
 })(document)
