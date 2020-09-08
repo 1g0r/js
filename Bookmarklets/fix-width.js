6520f599d4db73a6c8a2481ec7231a43fab36ec7
Игорь Логовской <igor@iMac-Igor.local>
2020-09-08 14:44:47 +0300

    Rm

--- Bookmarklets/fix-width.js
+++ Bookmarklets/fix-width.js
@@ -1,5 +1,5 @@
 (function(d){
-    var selector = '[class*=application],.l-table,l-window';
+    var selector = '[class*=application],.l-window';
     d.querySelectorAll(selector).forEach(e => {
         e.style.width = '100%';
         e.style.position = 'absolute';
