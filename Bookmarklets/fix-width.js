bb064eca01f872f3a427b75cd8bed802dfff6793
Игорь Логовской <igor@iMac-Igor.local>
2020-09-08 17:40:10 +0300

    Fix length

--- Bookmarklets/fix-width.js
+++ Bookmarklets/fix-width.js
@@ -4,13 +4,15 @@
     body.style.display = 'flex';
     body.style.flexDirection = 'column';
     body.style.justifyContent = 'center';
-    var elements = d.querySelectorAll(selector);
-    body.innerHTML = '';
-    elements.forEach(e => {
-        e.style.width = '100%';
-        e.style.maxWidth = '50%';
-        e.style.backgroundColor = 'white';
-        body.append(e);
-    });
+    var elements = d.querySelectorAll(selector) || [];
+    if(elements.length > 0){
+        body.innerHTML = '';
+        elements.forEach(e => {
+            e.style.width = '100%';
+            e.style.maxWidth = '50%';
+            e.style.backgroundColor = 'white';
+            body.append(e);
+        });
+    }
     alert('done');
 })(document)
