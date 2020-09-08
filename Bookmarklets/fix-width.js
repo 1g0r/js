c495081db8842e0356a2dcbd2ec4688ab4d59dd4
Игорь Логовской <igor@iMac-Igor.local>
2020-09-08 14:26:09 +0300

    Added class

--- Bookmarklets/fix-width.js
+++ Bookmarklets/fix-width.js
@@ -1,5 +1,5 @@
 (function(d){
-    var selector = '[class*=application]';
+    var selector = '[class*=application],.l-table';
     d.querySelectorAll(selector).forEach(e => e.style.width = '100%');
     alert('done');
 })(document)
