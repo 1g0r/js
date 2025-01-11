0be2b67be9dbeb03e1cd9e4020a103ab402c217c
1g0r <logovskoy@gmail.com>
2025-01-11 22:09:57 +0300

    Update remove-ad.js

--- Bookmarklets/remove-ad.js
+++ Bookmarklets/remove-ad.js
@@ -1,16 +1,19 @@
 (function(){
     var count = 0;
-    var selector = '.__brand_top_2889, \
-#mlph2889, \
-[class^="sidebar"]:not(html), \
-#fishki-video-frame-wrapper, \
-[class*="banner"], \
-iframe,\
-.pagelet, \
-iframe[src*="googleapis"], \
-.container-slider-top, \
-.sideColumn';
+    var selector = [
+        '.__brand_top_2889',
+        '#mlph2889',
+        '[class^="sidebar"]:not(html)',
+        '#fishki-video-frame-wrapper',
+        '[class*="banner"]',
+        'iframe',
+        '.pagelet',
+        'iframe[src*="googleapis"]',
+        '.container-slider-top',
+        '.sideColumn'
+    ].join(',');
     var els = document.querySelectorAll(selector);
+    alert(els.length + ' elements found')
     els.forEach(e => {
         if(!(e.src && e.src.length && e.src.length > 0)) {
             count++;
